d780125dbe251e468b3d0e454a761418055a35e1
dkm1891 <dkm1891@gmail.com>
2021-03-28 20:27:55 +0530

    Darshan - added code for ID filder , 3 output file willl gets generated

--- src/main/scala/com/fullcontact/interview/RecordFinder.scala
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -1,7 +1,63 @@
 package com.fullcontact.interview
 
+import org.apache.spark.sql.{ DataFrame, Dataset, Row, SparkSession }
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.SaveMode
+
 object RecordFinder {
+
+  def getMatchingRecords(spark: SparkSession, records: Dataset[String], queries: Dataset[String]): DataFrame =  {
+    import spark.implicits._
+
+    val recordsDF = records.select(split(col("value"), " ").as("records")).drop("value")
+
+    var result = queries.join(recordsDF, expr("array_contains(records,value)"))
+
+    result = result.withColumn("result", concat(col("value"), lit(":"), concat_ws(" ", col("records"))))
+
+    result
+      .drop("value", "records")
+      .repartition(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .text("matchingRecords")
+
+    val concatExpr = concat_ws(" ", flatten(collect_list("records")))
+
+    val resultUnion = result.groupBy(col("value"))
+      .agg(concat(col("value"), lit(":"), concatExpr).alias("result"))
+
+    resultUnion
+      .drop("value")
+      .repartition(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .text("matchingRecordsUnion")
+
+    val resultDistinct = result.groupBy(col("value"))
+      .agg(concat(col("value"), lit(":"), concat_ws(" ", array_distinct(split(concatExpr, " ")))).alias("result"))
+
+    resultDistinct
+      .drop("value")
+      .repartition(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .text("matchingRecordsDistinct")
+      
+    result.drop("value", "records")
+  }
+
   def main(args: Array[String]): Unit = {
-    // TODO: Implement your job here
+    val spark = SparkSession.builder().appName("FullContact Data Processing").master("local").getOrCreate()
+
+    import spark.implicits._
+
+    val records = spark.read.textFile("Records.txt")
+
+    val queries = spark.read.textFile("Queries.txt").filter("value == 'YSJNPIT'")
+
+    val outputDF = getMatchingRecords(spark, records, queries);
+
   }
 }
--- src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
+++ src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
@@ -1,9 +1,44 @@
 package com.fullcontact.interview
 
-import org.scalatest.{FunSuite, Matchers}
 
-class RecordFinderTest extends FunSuite with Matchers {
-  test("todo") {
-    pending
+import org.scalatest.FunSpec
+import com.github.mrpowers.spark.fast.tests.DataFrameComparer
+import org.apache.spark.sql.SparkSession
+
+trait SparkSessionTestWrapper {
+
+  lazy val spark: SparkSession = {
+    SparkSession.builder().appName("FullContact Data Processing").master("local").getOrCreate()
+  }
+
+}
+
+
+class DatasetSpec extends FunSpec with SparkSessionTestWrapper with DataFrameComparer {
+
+  import spark.implicits._
+
+  it("aliases a DataFrame") {
+
+    val records = Seq(
+      "YSJNPIT ORMJLXN OSMHWUF ANVLVIH",
+      "RERONRV VQZAHIV EKTDXLG YSJNPIT",
+      "JFOCIBQ WYQWIDV YSJNPIT GQMCOEC NSZGNUC"
+    ).toDS()
+
+    val query = Seq(
+      "YSJNPIT"
+    ).toDS()
+
+    val mainOutput = RecordFinder.getMatchingRecords(spark,records,query).toDF()
+
+    val expectedDF = Seq(
+      "YSJNPIT:YSJNPIT ORMJLXN OSMHWUF ANVLVIH",
+      "YSJNPIT:RERONRV VQZAHIV EKTDXLG YSJNPIT",
+      "YSJNPIT:JFOCIBQ WYQWIDV YSJNPIT GQMCOEC NSZGNUC"
+    ).toDF()
+
+    assertSmallDatasetEquality(mainOutput, expectedDF,orderedComparison = false)
+
   }
 }
